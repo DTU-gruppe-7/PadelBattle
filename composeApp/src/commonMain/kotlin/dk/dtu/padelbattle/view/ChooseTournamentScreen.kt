4df83e4b957aa93006f57850d8200daabacb36ff
Mathias Rasmussen <rasmussen_5@hotmail.com>
2026-01-07 12:58:55 +0100

    Fixed a small bug

--- composeApp/src/commonMain/kotlin/dk/dtu/padelbattle/view/ChooseTournamentScreen.kt
+++ composeApp/src/commonMain/kotlin/dk/dtu/padelbattle/view/ChooseTournamentScreen.kt
@@ -8,7 +8,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import dk.dtu.padelbattle.viewmodel.TournamentType
+import dk.dtu.padelbattle.model.TournamentType
 import dk.dtu.padelbattle.viewmodel.TournamentViewModel
 
 @Composable

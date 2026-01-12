01e79de1a38528c39214abb7eb627a0ed9ec5bfc
Mathias Rasmussen <rasmussen_5@hotmail.com>
2026-01-12 16:52:05 +0100

    Add platform-specific `formatDate` utility functions for date formatting

---
+++ composeApp/src/iosMain/kotlin/dk/dtu/padelbattle/model/utils/Utils.ios.kt
@@ -0,0 +1,17 @@
+package dk.dtu.padelbattle.model.utils
+
+import platform.Foundation.NSDate
+import platform.Foundation.NSDateFormatter
+import platform.Foundation.NSDateFormatterShortStyle
+import platform.Foundation.dateWithTimeIntervalSince1970
+
+actual fun formatDate(timestamp: Long): String {
+    // Konverter millisekunder (Kotlin) til sekunder (iOS)
+    val date = NSDate.dateWithTimeIntervalSince1970(timestamp / 1000.0)
+
+    val formatter = NSDateFormatter()
+    // ShortStyle giver typisk "dd/MM/yyyy" afhængigt af brugerens region
+    formatter.dateStyle = NSDateFormatterShortStyle
+
+    return formatter.stringFromDate(date)
+}
